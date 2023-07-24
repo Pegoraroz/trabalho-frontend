0983139ffd939771936e71c2b17ca5155586da1c
Pegoraroz <lucaspegoraro17@hotmail.com>
2023-07-24 19:50:58 -0300

    segundo

--- Trabalho/js/script.js
+++ Trabalho/js/script.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 1291256d0689cebfff86f94426369aeb349752ac
 function topo() {
     window.scrollTo({
         top: 0,
         left: 0,
         behavior: 'smooth'
-<<<<<<< HEAD
     });
 }
 
 function login() {
     var usuario = document.getElementsByName('usuario')[0].value.toLowerCase();
     var senha = document.getElementById('senha').value.toLowerCase();
-
-    // Recupera os dados armazenados no localStorage
     var storedUsuario = localStorage.getItem("usuario");
     var storedSenha = localStorage.getItem("senha");
 
@@ -31,13 +24,11 @@ function cadastro() {
     var usuario = document.getElementsByName('usuario')[0].value.toLowerCase();
     var senha = document.getElementById('senha').value.toLowerCase();
 
-    // Salva os dados do usuário no localStorage
     localStorage.setItem("usuario", usuario);
     localStorage.setItem("senha", senha);
 
     alert("Cadastrado com sucesso!");
 
-    // Redireciona à tela de login após o cadastro
     window.location.replace("login.html");
 }
 
@@ -63,30 +54,3 @@ document.getElementById('cadastro-form').addEventListener('submit', function (ev
 });
 
 checkLogin();
-=======
-    })
-}
-
-function login() {
-    var logado = 0;
-    var usuario = document.getElementsByName('usuario')[0].value;
-    usuario = usuario.toLowerCase();
-    var senha = document.getElementsById('senha').value;
-    senha -senha.toLowerCase(); 
-
-    if(usuario == "admin" && senha =="12345"){
-        window.location = "home.html";
-        logado = 1;
-    }
-    
-    if(logado == 0){
-        alert("Acesso Negado. Dados incorretos");
-    }
-
-}
-
-function cadastro() {
-    alert("Cadastrado com sucesso!");
-    window.location.href = "home.html";
-}
->>>>>>> 1291256d0689cebfff86f94426369aeb349752ac
